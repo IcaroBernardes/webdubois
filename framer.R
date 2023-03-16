03633da7d7ac142b7eca6870ba4481fc560cafe7
Icaro Bernardes <asaicaro@gmail.com>
2023-03-15 23:36:50 -0300

    new layout and more

--- framer.R
+++ framer.R
@@ -6,15 +6,24 @@ library(magick)
 library(purrr)
 library(stringr)
 
-# 0. Images adjustment ##########
 ## Lists all original images
-original_images <- list.files(path = "originals", pattern = ".jpg", full.names = TRUE)
+original_images <- list.files(path = "originals", pattern = ".png", full.names = TRUE)
 
 ## Lists all new images
 new_images <- list.files(recursive = TRUE, pattern = ".png", full.names = TRUE)
 new_images <- new_images |> 
   stringr::str_subset("week[:digit:]{2}") |> 
   stringr::str_subset("_pt.png|week00", negate = TRUE)
+
+# 1. Images handling ##########
+## Defines the final dimensions of thumbnails
+dim_num <- 200
+dim_str <- as.character(dim_num)
+
+## Creates a new image with white background and a black circle
+mask <- magick::image_draw(magick::image_blank(dim_num, dim_num))
+symbols(dim_num/2, dim_num/2, circles=(dim_num/2)-3, bg="black", inches=FALSE, add=TRUE)
+dev.off()
 
 ## Creates a function that adjusts the images
 adjuster <- function(img, final_path, thumb) {
@@ -26,26 +35,44 @@ adjuster <- function(img, final_path, thumb) {
   
   ### Defines final dimensions
   if (thumb) {
-    h_std = 128
-    w_std = 100
+    h_std = dim_num
+    w_std = dim_num
   } else {
     h_std = 700
     w_std = 550
   }
   
   ### Defines the scale geometry
-  if (h/w > 1.272727) {
+  if (thumb) {
     geom_scales = glue::glue("{w_std}x")
   } else {
-    geom_scales = glue::glue("x{h_std}")
+    if (h/w > 1.272727) {
+      geom_scales = glue::glue("{w_std}x")
+    } else {
+      geom_scales = glue::glue("x{h_std}")
+    }
   }
   
-  ### Manipulates the image and saves
-  img |> 
+  ### Manipulates the image
+  img = img |> 
     magick::image_scale(geometry = geom_scales) |> 
     magick::image_crop(geometry = glue::glue("{w_std}x{h_std}"),
-                       gravity = "Center") |> 
-    magick::image_write(final_path)
+                       gravity = "Center")
+  
+  ### Creates circular versions for the thumbnails
+  if (thumb) {
+    #### Creates an image composite using the image and the circular mask
+    img = magick::image_composite(img, mask, operator = "copyopacity")
+    
+    #### Sets the background as transparent
+    img = magick::image_background(img, "transparent")
+  }
+  
+  ### Saves the image
+  magick::image_write(img, final_path)
+  
+  ### Forces the memory cleaning process
+  gc(verbose = FALSE)
   
 }
 
@@ -59,7 +86,6 @@ original_images |>
     ### Creates the path to the final image
     path = stringr::str_remove(path, "originals/")
     final_path = glue::glue("www/dubois/{path}")
-    
     
     ### Adjusts the image
     adjuster(img, final_path, FALSE)
